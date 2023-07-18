22f1f3a10874578fc066b8a250166ed49d6c16c3
sorhawell <sorhawell@gmail.com>
2023-07-18 10:32:16 +0200

    simplify

--- extendr-polars/src/lib.rs
+++ extendr-polars/src/lib.rs
@@ -102,22 +102,9 @@ impl WrapDataFrame {
         rprintln!("in hex it is: {:x}", y);
         let rx = s_ptr.into_robj();
 
-        let robj: Robj = R!("polars:::import_arrow_array_stream({{rx}})")?;
-        dbg!(&robj);
+        let robj = R!("polars:::import_arrow_array_stream({{rx}}) |> (\\(x) if(is.null(x$err)) x$ok else stop(x$err))()");
 
-        let l = robj.as_list().unwrap();
-        let e1: Robj = l.elt(0)?.as_list().unwrap().elt(0)?;
-        //let _e2 = l.elt(1)?.as_str().unwrap();
-
-        // let x: usize = e2.parse().expect("input is a pointer value");
-
-        // //let stream = x as *mut ffi::ArrowArrayStream;
-        // //let boxed_stream = Box::new(unsafe { *stream });
-        // let x = x as *mut ffi::ArrowArrayStreamReader<dyn >;
-        // let x = unsafe { Box::from_raw(x) };
-        // drop(x);
-
-        Ok(e1)
+        robj
     }
 }
 
